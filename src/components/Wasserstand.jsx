096f26ecb3734699faf44e19e68085aa72d883b1
Robinho1997 <robin.kempen@gmx.de>
2023-07-11 07:07:48 +0200

    Tilt

--- src/components/Wasserstand.jsx
+++ src/components/Wasserstand.jsx
@@ -1,9 +1,22 @@
+import { Tilt } from "react-tilt";
+
+const defaultOptions = {
+  reverse: false,
+  max: 35, 
+  perspective: 1000, 
+  scale: 1.1, 
+  speed: 1000, 
+  transition: true, 
+  axis: null, 
+  reset: true, 
+  easing: "cubic-bezier(.03,.98,.52,.99)", 
+};
 
 const Wasserstand = (props) => (
-    <div>
-      <p>{props.name}</p>
-      <p>{props.wasserstand}</p>
-    </div>
-  );
+  <Tilt options={defaultOptions}   className="bg-tertiary p-5 rounded-2xl w-[325px] h-[325px] text-white">
+    <p >{props.name}</p>
+    <p>{props.wasserstand}</p>
+  </Tilt>
+);
 
-  export default Wasserstand
+export default Wasserstand;
