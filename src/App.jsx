3533d515726181f11abcac7209b80569f980dfc0
Robinho1997 <robin.kempen@gmx.de>
2023-07-12 06:11:14 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import FetchWasserstände from "./components/fetchWasserstände"
+import FetchWasserstände from "./components/fetchWasserstände";
 
 function App() {
- 
-
   return (
-  <div>
-    <FetchWasserstände/>
-  </div>
-  )
+    <div>
+      <h1 className="text-white bg-tertiary text-center absolute top-0 w-full  p-2">
+        Wassertstände Rhein Umkreis Siebengebirge
+      </h1>
+      <FetchWasserstände />
+    </div>
+  );
 }
-
-export default App
+export default App;
