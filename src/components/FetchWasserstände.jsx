6f090bd4d46a905f33f725a437a4befa419b89ff
Robinho1997 <robin.kempen@gmx.de>
2023-07-12 06:12:53 +0200

    Wrapper div

--- src/components/FetchWasserstände.jsx
+++ src/components/FetchWasserstände.jsx
@@ -3,7 +3,7 @@ import Wasserstand from "./Wasserstand";
 
 function FetchWasserstände() {
   const [elements, setElements] = useState([]);
-  console.log(elements);
+
   useEffect(() => {
     fetch(
       "https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations.json?includeTimeseries=true&includeCurrentMeasurement=true&includeCharacteristicValues=true&waters=RHEIN"
@@ -22,7 +22,7 @@ function FetchWasserstände() {
           ) {
             let name = item.shortname;
             let wasserstand =
-              item.timeseries[0].currentMeasurement.value + " cm";
+              item.timeseries[0].currentMeasurement.value;
             arr.push({ name, wasserstand });
           }
           setElements(arr);
@@ -31,10 +31,12 @@ function FetchWasserstände() {
   }, []);
 
   return (
-    <div>
+    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex flex-wrap gap-[25px] justify-center mt-[50px]">
       {elements.map((el, index) => (
         <Wasserstand key={index} name={el.name} wasserstand={el.wasserstand} />
       ))}
+    </div>
     </div>
   );
 }
