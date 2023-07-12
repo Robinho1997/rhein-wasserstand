9c64fad6540bce73f2270adcca4433909283268f
Robinho1997 <robin.kempen@gmx.de>
2023-07-12 06:10:32 +0200

    Fetch Weather

--- src/components/Wasserstand.jsx
+++ src/components/Wasserstand.jsx
@@ -1,22 +1,77 @@
+import { useEffect, useState } from "react";
 import { Tilt } from "react-tilt";
 
 const defaultOptions = {
   reverse: false,
-  max: 35, 
-  perspective: 1000, 
-  scale: 1.1, 
-  speed: 1000, 
-  transition: true, 
-  axis: null, 
-  reset: true, 
-  easing: "cubic-bezier(.03,.98,.52,.99)", 
+  max: 35,
+  perspective: 1000,
+  scale: 1.1,
+  speed: 1000,
+  transition: true,
+  axis: null,
+  reset: true,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
 };
 
-const Wasserstand = (props) => (
-  <Tilt options={defaultOptions}   className="bg-tertiary p-5 rounded-2xl w-[325px] h-[325px] text-white">
-    <p >{props.name}</p>
-    <p>{props.wasserstand}</p>
-  </Tilt>
-);
+const Wasserstand = (props) => {
+  const [temperature, setTemperature] = useState();
+  const [wind, setWind] = useState();
+  const [icon, setIcon] = useState();
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${props.name}&appid=&units=metric
+    `)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTemperature(data.main.temp);
+        setWind(data.wind.speed);
+        setIcon(data.weather[0].icon);
+      });
+  }, []);
+
+  return (
+    <Tilt
+      options={defaultOptions}
+      className="bg-tertiary p-5 rounded-2xl w-[300px] h-[300px] text-white flex flex-col justify-between align-center"
+    >
+      <div className="flex justify-between">
+        <div>
+          <p>{props.name}</p>
+          <p>{props.wasserstand} cm</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <p>{Math.floor(temperature)} &deg;</p>
+          <img src={`http://openweathermap.org/img/w/${icon}.png`} />
+        </div>
+      </div>
+
+      <p
+        className={`border-gray-700 border rounded-xl w-[250px] h-[25px] ${
+          props.wasserstand < 200 ? "" : "bg-cyan-900"
+        }`}
+      ></p>
+      <p
+        className={`border border-gray-700 rounded-xl w-[250px] h-[25px] ${
+          props.wasserstand < 150 ? "" : "bg-cyan-700"
+        }`}
+      ></p>
+      <p
+        className={`border border-gray-700 rounded-xl w-[250px] h-[25px] ${
+          props.wasserstand < 100 ? "" : "bg-cyan-600"
+        }`}
+      ></p>
+      <p
+        className={`border border-gray-700 rounded-xl w-[250px] h-[25px] ${
+          props.wasserstand < 80 ? "" : "bg-cyan-400"
+        }`}
+      ></p>
+      <p
+        className={`border border-gray-700 rounded-xl w-[250px] h-[25px] ${"bg-cyan-200"}`}
+      ></p>
+    </Tilt>
+  );
+};
 
 export default Wasserstand;
